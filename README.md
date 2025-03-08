5c23f2579f485b39335b7d4af14644c07ed68456
baharlawter <baharlawter@gmail.com>
2025-03-08 09:01:45 -0600

    Inital commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # practice-redline-syng-Bahar-Lawter-BL
+
+
+Bahar's first ReadLineSync Practice
+
---
+++ ReadLine.js
@@ -0,0 +1,12 @@
+const readLineSync=require("readline-sync");
+let question1=readLineSync.question("What is your name?");
+let question2=readLineSync.question("What is your age?");
+let question3=readLineSync.question("What is your address?");
+let questiont4=readLineSync.question("What is your phone number?");
+let question5=readLineSync.question("What is your email address?");
+
+console.log("Your name is"+""+question1);
+console.log("Your age is "+ ""+question1);
+console.log("Your address is "+ "" +question3);
+console.log("Your phone number is "+""+question4);
+console.log("Your email address is"+""+question5);
